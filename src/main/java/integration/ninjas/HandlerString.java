90cb4ea3fdbcce37437f9654c59a8a6d64080a04
integrationninjas <integrationninjas@gmail.com>
2023-10-15 19:09:35 +0530

    initial commit for aws lambda

---
+++ src/main/java/integration/ninjas/HandlerString.java
@@ -0,0 +1,19 @@
+package integration.ninjas;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class HandlerString implements RequestHandler<String, String>{
+
+    @Override
+    /*
+     * Takes a String as input, and converts all characters to lowercase.
+     */
+    public String handleRequest(String event, Context context)
+    {
+        LambdaLogger logger = context.getLogger();
+        logger.log("EVENT TYPE: " + event.getClass().toString());
+        return event.toLowerCase();
+    }
+}
